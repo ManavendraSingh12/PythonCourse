3fe544419ba45cba8896cd9777aa6948ff801ac2
ManavendraSingh12 <92295942+ManavendraSingh12@users.noreply.github.com>
2022-12-19 14:33:14 +0530

    Modified lists.py

--- Basics/lists.py
+++ Basics/lists.py
@@ -41,6 +41,16 @@ numbers.pop() # Removes last element
 
 print(numbers) 
 
+# Tuples are similar to lists but they are immutable (Can't modify)
+
+subjects = ("math","sci","eng")
+print(type(subjects))
+
+# subjects[0] = "social"  Will throw an error
+
+print(len(subjects)) # Prints the length of the tuple
+del subjects # Deletes the tuple completely
+print(subjects)
 
 
 
